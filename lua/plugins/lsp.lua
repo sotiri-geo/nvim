110173917c74a6283287db87f4d74b12dd8a1d48
sotiri <sotiri@thoughtmachine.net>
2026-05-15 15:44:04 +0100

    feat: added lang servers

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,52 @@
+-- Installing language servers
+require("mason").setup()
+
+-- Ensure language server binaries exist
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "gopls",
+        "lua_ls",
+        "pyright",
+    },
+})
+
+-- Lua server
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnosticr = {
+                globals = { "vim" },
+            },
+            workspace = {
+                Library = {
+                    vim.env.VIMRUNTIME,
+                },
+            },
+        },
+    },
+})
+
+-- Go server
+vim.lsp.config("gopls", {
+    settings = {
+        gopls = {
+            gofumpt = true,
+            staticcheck = true,
+            analyses = {
+                unusedparams = true,
+                nilness = true,
+                unusedwrite = true,
+            },
+        },
+    },
+})
+
+-- Python server
+vim.lsp.config("pyright", {})
+
+-- Add to the list of servers if required
+vim.lsp.enable({
+    "gopls",
+    "lua_ls",
+    "pyright",
+})
