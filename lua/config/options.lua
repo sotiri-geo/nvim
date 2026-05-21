9ac46e935dfd36c2e62cafa98f91a3152757d4aa
sotiri <sotiri@thoughtmachine.net>
2026-05-21 14:59:29 +0100

    feat: autorefresh after buffer file has been updates externally

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,11 @@ vim.opt.tabstop = 4
 vim.opt.termguicolors = true
 vim.opt.hidden = true
 vim.opt.hlsearch = false
+vim.opt.autoread = true
+
+-- Force Neovim to read from disk: This can happen when we've run the linter that changes 
+-- the buffered file, such that it refreshes automatically
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    pattern = "*",
+    command = "if mode() !~ '\\[vV\x16\\]' && getcmdwintype() == '' | checktime | endif",
+})
