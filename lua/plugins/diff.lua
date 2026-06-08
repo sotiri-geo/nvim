939e52cfcb3ed0350021a4730e671c84adfa1bb4
sotiri <sotiri@thoughtmachine.net>
2026-06-08 19:51:15 +0100

    fix: making diff more noticeable

--- lua/plugins/diff.lua
+++ lua/plugins/diff.lua
@@ -1,1 +1,6 @@
 require("mini.diff").setup()
+
+-- Make diff more noticeable
+vim.api.nvim_set_hl(0, 'MiniDiffSignChange', { fg = '#e5c07b', bold = true })
+vim.api.nvim_set_hl(0, 'MiniDiffSignDelete', { fg = '#e06c75', bold = true })
+vim.api.nvim_set_hl(0, 'MiniDiffSignAdd', { fg = '#98c379', bold = true })
