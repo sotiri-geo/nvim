ec36d5c071fd2c46952e3e5a542eb260267db5b1
sotiri <sotiri@thoughtmachine.net>
2026-07-30 08:21:37 +0100

    fix: using vim keymaps to jump up/down in file explorer

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -19,3 +19,25 @@ minifiles.setup({
         use_as_default_explorer = true,
     },
 })
+
+-- Map keys dynamically when a mini.files buffer is created
+vim.api.nvim_create_autocmd("User", {
+    pattern = "MiniFilesBufferCreate",
+    callback = function(args)
+        local buf_id = args.data.buf_id
+
+        -- Helper to scroll active directory list by moving cursor
+        local scroll_explorer = function(direction)
+            local win = vim.api.nvim_get_current_win()
+            local height = vim.api.nvim_win_get_height(win)
+            local lines = math.floor(height / 4)
+            local key = direction == "down" and "j" or "k"
+            -- Move cursor to trigger mini.files update events cleanly
+            vim.cmd("normal! " .. lines .. key)
+        end
+
+        vim.keymap.set("n", "<C-d>", function() scroll_explorer("down") end, { buffer = buf_id, desc = "Scroll explorer down" })
+        vim.keymap.set("n", "<C-u>", function() scroll_explorer("up") end, { buffer = buf_id, desc = "Scroll explorer up" })
+    end,
+})
+
