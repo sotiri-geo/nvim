e0253942fa2851d695317222e561013f6cd11329
Sotiri Georgiou <tirigeorgiou@gmail.com>
2026-08-26 08:32:20 +0100

    fix: removing timeout

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,5 +13,5 @@ vim.opt.termguicolors = true
 vim.opt.hidden = true
 vim.opt.hlsearch = false
 vim.opt.autoread = true
-vim.opt.timeoutlen = 700
-vim.opt.timeout = true
+-- vim.opt.timeoutlen = 700
+vim.opt.timeout = false
